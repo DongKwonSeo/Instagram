544e4fc10b9f33740b5f2188690082fa364689b9
timebear000 <timebear000@gmail.com>
2021-08-04 23:10:48 +0900

    Feat: Model Content 생성

---
+++ back-end/Model/Content.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const getCurrentDate = require("../util/CurrentTime");
+const ContentSchema = new mongoose.Schema(
+  {
+    user_nickname: {
+      type: String,
+      required: [true, "유저 닉네임은 필수입니다."],
+    },
+    image: {
+      type: String,
+      required: [true, "게시글 이미지는 필수입니다."],
+    },
+
+    text: {
+      type: String,
+      required: [true, "게시글은 필수입니다."],
+    },
+    like: {
+      type: Number,
+      default: 0,
+    },
+    cratedAt: {
+      type: Date,
+      default: getCurrentDate("currentTime"),
+    },
+    updatedAt: {
+      type: Date,
+      default: getCurrentDate("currentTime"),
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+module.exports = mongoose.model("contents", ContentSchema);
