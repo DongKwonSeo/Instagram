76856d088e243e0ab4fc702bffc5a540e044f5ba
DongKwon <gun100491@gmail.com>
2021-08-09 13:51:29 +0900

    Feat: Instargram 시작

---
+++ front-end/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import "./scss/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="hello">hello</h1>
+    </div>
+  );
+}
+
+export default App;
