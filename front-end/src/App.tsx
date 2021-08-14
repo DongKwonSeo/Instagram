655dfe69ebcfafc6f0d3d1f2576d3486f1058e99
DongKwon <gun100491@gmail.com>
2021-08-15 05:13:10 +0900

    Feat: Router 추가

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import FeedList from "./components/main/feed/feedList";
 import Header from "./components/main/header/header";
 import StoryList from "./components/main/story/storyList";
-// import Post from "./components/post/post";
+import Post from "./components/post/post";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./scss/style.scss";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="app">
-      <Header />
-      <StoryList />
-      <FeedList />
-      {/* <Post /> */}
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <div className="app">
+          <Route exact path="/" component={Header} />
+          <Route exact path="/" component={StoryList} />
+          <Route exact path="/" component={FeedList} />
+          <Route exact path="/post" component={Post} />
+          {/* <Post /> */}
+        </div>
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
